b7ab284dda050487a99f60e414b41b23b39d99d1
Muzammil Kabier <137934634+Muzatheking12@users.noreply.github.com>
2024-02-28 22:15:30 +0530

    Update my.bat

--- MzDOCK/G/ensemblesplit/my.bat
+++ MzDOCK/G/ensemblesplit/my.bat
@@ -1,7 +1,7 @@
 @echo off
 set "file= %~1"
 
-"C:\Users\User\Desktop\mz\G\hey\python.exe" pdb_splitmodel.py %file%
+"..\hey\python.exe" pdb_splitmodel.py %file%
 
 del %file% 2>nul
 move ".\*.pdb" ".\files" > nul
